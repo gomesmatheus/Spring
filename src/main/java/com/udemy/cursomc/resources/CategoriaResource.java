fc5cfb3506221370812e27c28dc078df27004889
Matheus Gomes <gom.matheus@hotmail.com>
2020-03-18 17:36:45 -0300

    Criando repository e service para categoria

--- src/main/java/com/udemy/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/udemy/cursomc/resources/CategoriaResource.java
@@ -1,26 +1,26 @@
 package com.udemy.cursomc.resources;
 
-import java.util.Arrays;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.cursomc.domain.Categoria;
+import com.udemy.cursomc.services.CategoriaService;
 
 @RestController // INDICA QUE É UM CONTROLADOR REST
 @RequestMapping(value = "/categorias") // ENDPOINT
 public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET) // INFORMA QUAL MÉTODO DE REQUISIÇÃO
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = Arrays.asList(cat1, cat2);
-		
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="{id}", method = RequestMethod.GET) // INFORMA QUAL MÉTODO DE REQUISIÇÃO
+	// ResponseEntity encapsula as respostas necessárias para um serviço rest
+	public ResponseEntity<?> find(@PathVariable Integer id) { // ANOTAÇÃO @PathVariable indica que a variável recebida no parametro, deve ser a variável passada no path do endpoint
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
