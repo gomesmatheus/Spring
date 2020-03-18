dd361457224b2da1bd13b912dfb051f53a16121d
Matheus Gomes <gom.matheus@hotmail.com>
2020-03-18 18:35:49 -0300

    Ajustes endpoint categorias/id

--- src/main/java/com/udemy/cursomc/domain/Produto.java
+++ src/main/java/com/udemy/cursomc/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -22,6 +24,7 @@ public class Produto implements Serializable{
 	private String nome;
 	private Double preco;
 	
+	@JsonBackReference // Do outro lado da associação, ele mostra a lista de categorias, pois ela possui a anotação @JsonManagedReference
 	@ManyToMany
 	@JoinTable(
 			name = "PRODUTO_CATEGORIA", 
